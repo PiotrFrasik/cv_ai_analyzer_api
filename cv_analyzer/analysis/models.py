38bcc8854302e1dff9c6e4c9ac34852cb7ae5948
PiotrFrasik <frasik.piotrek@gmail.com>
2026-08-19 20:46:13 +0200

    initialize analisys model for AI

--- cv_analyzer/analysis/models.py
+++ cv_analyzer/analysis/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from documents.models import CV, JobOffer
 
-# Create your models here.
+class Analysis(models.Model):
+    cv = models.ForeignKey(
+        CV,
+        on_delete=models.CASCADE,
+        related_name = "analysis_cvs"
+    )
+    job_offer = models.ForeignKey(
+        JobOffer,
+        on_delete=models.CASCADE,
+        related_name = "analysis_job_offers"
+    )
+
+    class Status(models.TextChoices):
+        PENDING = "pending", "Pending"
+        PROCESSING = "processing", "Processing"
+        DONE = "done","Done"
+        FAILED = "failed","Failed"
+
+
+    status = models.CharField(
+        choices=Status.choices,
+        default="pending",
+        max_length=10
+    )
+
+    match_score = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        null=True,
+    )
+    
