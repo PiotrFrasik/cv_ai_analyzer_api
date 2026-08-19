b54f58ab4002da578ec86c3e4748e4a93cc7c51b
PiotrFrasik <frasik.piotrek@gmail.com>
2026-08-19 19:54:51 +0200

    implement custom user model

--- cv_analyzer/cv_analyzer/settings.py
+++ cv_analyzer/cv_analyzer/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'analysis',
+    'documents',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +128,5 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+AUTH_USER_MODEL = 'users.CustomUser'
--- cv_analyzer/users/models.py
+++ cv_analyzer/users/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(unique=True,
+                                        null=False,
+                                        max_length=12 #ex. 123 456 789
+                                        )
+
+    class Role(models.TextChoices):
+        CANDIDATE = "candidate"
+        RECRUITER = "recruiter"
+
+    role = models.CharField(blank=False, choices=Role.choices, max_length=10)
+
+class CandidateProfile(models.Model):
+    user = models.OneToOneField(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='candidate_profile'
+    )
+
+    salary_min = models.IntegerField()
+    salary_max = models.IntegerField()
+
+    city = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)
+    
+class RecruiterProfile(models.Model):
+    user = models.OneToOneField(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='recruiter_profile'
+    )
+    department = models.CharField(max_length=30)
