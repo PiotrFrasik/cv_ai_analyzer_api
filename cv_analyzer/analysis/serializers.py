bcb3794ea27a7f8eb936cec74cf68e397ca9b265
PiotrFrasik <frasik.piotrek@gmail.com>
2026-08-22 15:58:25 +0200

    add serializers Analysis models

--- cv_analyzer/analysis/serializers.py
+++ cv_analyzer/analysis/serializers.py
@@ -5,6 +5,15 @@ class AnalysisDetailSerializer(serializers.ModelSerializer):
     """
     Displaying CV analysis results.
     """
+    
     class Meta:
         model = Analysis
         fields = ['cv', 'job_offer', 'status', 'match_score']
+
+    def to_representation(self, instance):
+        return{
+            'CV': instance.cv.owner.username,
+            'job_offer': instance.job_offer.title,
+            'status': instance.status,
+            'match_score': instance.match_score
+        }
