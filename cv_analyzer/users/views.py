f17280f62914f042dd82cbe3264f7d2280672076
PiotrFrasik <frasik.piotrek@gmail.com>
2026-08-22 20:00:29 +0200

    implement user registration with url

--- cv_analyzer/users/views.py
+++ cv_analyzer/users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import CustomUser
+from .serializers import RegisterSerializer
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+class RegisterAPIView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
