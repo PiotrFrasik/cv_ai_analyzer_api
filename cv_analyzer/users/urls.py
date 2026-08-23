cc9c6e10ce145be4351621bf9815f61d84e3c347
PiotrFrasik <frasik.piotrek@gmail.com>
2026-08-23 14:36:10 +0200

    implement profile retrieval API endpoints

--- cv_analyzer/users/urls.py
+++ cv_analyzer/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from .views import RegisterAPIView
+from .views import RegisterAPIView, UserProfileAPIView
 
 urlpatterns = [
-    path('register/', RegisterAPIView.as_view(), name='register_users')
+    path('register/', RegisterAPIView.as_view(), name='register_users'),
+    path('profile/', UserProfileAPIView.as_view(), name='profile_user')
 ]
--- cv_analyzer/users/views.py
+++ cv_analyzer/users/views.py
@@ -1,10 +1,19 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from .models import CustomUser
-from .serializers import RegisterSerializer
-from rest_framework.permissions import AllowAny
+from .serializers import RegisterSerializer, UserProfileSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated 
 
 class RegisterAPIView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
 
+class UserProfileAPIView(generics.RetrieveAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        # Override to return the currently authenticated user instead of a URL lookup
+        return self.request.user
