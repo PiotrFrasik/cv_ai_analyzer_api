dd5851529469864fff8c35c6df707181c3e9ade0
Piotr Frasik <frasik.piotrek@gmail.com>
2026-09-08 22:27:12 +0200

    automatically create analyses matching CVs and job offers by department

--- cv_analyzer/documents/views.py
+++ cv_analyzer/documents/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated 
 
 from .models import CV, JobOffer
+from analysis.models import Analysis
 from .serializers import CVCreateSerializer, JobOfferCreateSerializer, CVDetailSerializer, JobOfferDetailSerializer
 from .permissions import IsCVOwnerOrRecruiter
 
@@ -11,7 +12,12 @@ class CVCreateAPIView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+        cv = serializer.save(owner=self.request.user)
+        if hasattr(self.request.user, 'candidate_profile'):
+            preferred_department = self.request.user.candidate_profile.preferred_department
+            offers = JobOffer.objects.filter(owner__recruiter_profile__department=preferred_department)
+            analyses = [Analysis(cv=cv, job_offer=offer) for offer in offers]
+            Analysis.objects.bulk_create(analyses)
 
 class JobOfferCreateAPIView(generics.CreateAPIView):
     queryset = JobOffer.objects.all()
@@ -19,7 +25,12 @@ class JobOfferCreateAPIView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+        job_offer = serializer.save(owner=self.request.user)
+        if hasattr(self.request.user, 'recruiter_profile'):
+            department = self.request.user.recruiter_profile.department
+            cvs = CV.objects.filter(owner__candidate_profile__preferred_department=department)
+            analyses = [Analysis(cv=cv, job_offer=job_offer) for cv in cvs]
+            Analysis.objects.bulk_create(analyses)
 
 class CVDetailAPIView(generics.RetrieveAPIView):
     queryset = CV.objects.all()
