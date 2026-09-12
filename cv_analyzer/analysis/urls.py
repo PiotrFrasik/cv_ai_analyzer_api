28bdc106469858160dc64a52e989bf20523c32f3
Piotr Frasik <frasik.piotrek@gmail.com>
2026-09-12 10:46:55 +0200

    add analysis list API endpoints

--- cv_analyzer/analysis/urls.py
+++ cv_analyzer/analysis/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import AnalysisDetailAPIView
+from .views import AnalysisDetailAPIView, AnalysisListAPIVIew
 urlpatterns = [
     path('analysis/<int:pk>/', AnalysisDetailAPIView.as_view(), name='analysis_detail'),
+    path('analysis', AnalysisListAPIVIew.as_view(), name='analysis_list'),
 ]
--- cv_analyzer/analysis/views.py
+++ cv_analyzer/analysis/views.py
@@ -1,11 +1,22 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated 
+from django.db.models import Q
 
 from .models import Analysis
 from .serializers import AnalysisDetailSerializer
 from .permissions import IsRecruiterOrCVOwner
 
 class AnalysisDetailAPIView(generics.RetrieveAPIView):
+    """Retrieves a single analysis instance for the CV owner or the associated recruiter."""
     queryset = Analysis.objects.all()
     serializer_class = AnalysisDetailSerializer
     permission_classes = [IsAuthenticated, IsRecruiterOrCVOwner]
+
+class AnalysisListAPIVIew(generics.ListAPIView):
+    """Lists all analyses where the authenticated user is either the CV owner or the job offer owner."""
+    serializer_class = AnalysisDetailSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Analysis.objects.filter(Q(cv__owner=user) | Q(job_offer__owner=user))
